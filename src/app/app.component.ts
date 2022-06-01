e270263aa9dc3dd293978dd8842cdc0fa9b4c9e5
Tony Schroeder <tony.schroeder@gmx.de>
2022-06-01 10:10:41 +0200

    add slide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TS-Agular.de';
+  slideImages = ['image-slide (1).jpg','image-slide (2).jpg','image-slide (3).jpg'];
+  currendImage = 0;
+
+ngOnInit() {
+  // this.UpdateImage();
+}
+
+
+// UpdateImage() {
+
+// setInterval(() => {
+//   this.currendImage++;
+//   this.currendImage = this.currendImage % this.slideImages.length;
+// }, 5000);
+
+// }
+
+
+
 }
